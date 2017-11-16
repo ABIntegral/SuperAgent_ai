c2718ca7e7d1c6ae2d7f7a11eaff8ed0044b8067
ABIntegral <32178046+ABIntegral@users.noreply.github.com>
2017-11-16 19:35:35 +0200

    Add files via upload

---
+++ Scripts/Agent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+public class Agent {
+	public virtual void SendParameters(EnvironmentParameters env) {
+
+	}
+
+	public virtual string Receive() {
+		return "";
+	}
+
+	public virtual float[] GetAction() {
+		return new float[1] { 0.0f };
+	}
+
+	public virtual float[] GetValue() {
+		float[] value = new float[1];
+		return value;
+	}
+		
+	public virtual void SendState(List<float> state, float reward, bool d) {
+
+	}
+}
